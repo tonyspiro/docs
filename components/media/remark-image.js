e736c40e78f72cf3e573976a1959d428b4671fcc
William Li <email@williamli.dev>
2020-07-15 01:25:03 +0800

    use Amp-Img in RemarkImage

--- components/media/remark-image.js
+++ components/media/remark-image.js
@@ -1,5 +1,201 @@
+import React, { Component } from 'react'
+import { useAmp } from 'next/amp'
+import PropTypes from 'prop-types'
+import IObserver from '~/components/intersection-observer'
+import cn from 'classnames'
+
+import VideoComponent from './video'
+
 import RemarkCaption from '~/components/text/remark-caption'
 import components from '~/lib/remark-components'
+
+// This component might look a little complex
+// because one could argue that keeping the aspect ratio
+// of an image can be solved with `height: auto`,
+// but it's actually not that easy if you want to prevent
+// element flickering
+
+// Because if you want to do that, you need to set the aspect
+// ratio of the image's container BEFORE the image loads
+
+const AmpImg = ({
+  children,
+  src,
+  srcSet,
+  height,
+  width,
+  alt,
+  layout = 'responsive'
+}) => {
+  const isAmp = useAmp()
+
+  if (isAmp)
+    return <amp-img layout={layout} {...{ src, srcSet, height, width, alt }} />
+  return children
+}
+
+class Image extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  static defaultProps = {
+    lazy: true
+  }
+
+  static propTypes = {
+    width: PropTypes.number,
+    height: PropTypes.number
+  }
+
+  state = {
+    src: !this.props.lazy ? this.props.videoSrc || this.props.src : undefined
+  }
+
+  handleIntersect = entry => {
+    if (entry.isIntersecting) {
+      this.setState({ src: this.props.src })
+    }
+  }
+
+  render() {
+    const {
+      caption,
+      height,
+      lazy,
+      margin = 40,
+      video = false,
+      videoSrc,
+      width,
+      captionSpacing = null,
+      oversize = false,
+      borderRadius = false,
+      children,
+      shadow,
+      align = 'center',
+      layout,
+      title,
+      alt
+    } = this.props
+
+    const aspectRatio = height
+      ? String((height / width) * 100) + '%'
+      : undefined
+    const classes = width > 768 && oversize ? 'oversize' : ''
+
+    if (video || videoSrc) {
+      return <VideoComponent src={videoSrc} {...this.props} />
+    }
+
+    return (
+      <AmpImg layout={layout} {...this.props}>
+        <IObserver
+          once
+          onIntersect={this.handleIntersect}
+          rootMargin="20%"
+          disabled={!lazy}
+        >
+          <figure className={cn(classes, { 'has-shadow': shadow })}>
+            <main style={{ width }}>
+              <div
+                className="container"
+                style={{ paddingBottom: aspectRatio ? aspectRatio : undefined }}
+              >
+                {this.state.src ? (
+                  <img
+                    src={this.state.src || null}
+                    width={width}
+                    height={height}
+                    title={title}
+                    alt={alt}
+                  />
+                ) : (
+                  children
+                )}
+              </div>
+
+              {caption && (
+                <p style={captionSpacing ? { marginTop: captionSpacing } : {}}>
+                  {caption}
+                </p>
+              )}
+            </main>
+
+            <style jsx>{`
+              figure {
+                display: block;
+              }
+
+              main {
+                max-width: 100%;
+              }
+
+              div {
+                position: relative;
+              }
+
+              img {
+                height: ${height ? '100%' : 'auto'};
+                left: 0;
+                position: ${height ? 'absolute' : 'relative'};
+                top: 0;
+                width: unset;
+                max-width: 100%;
+              }
+
+              .container {
+                display: flex;
+                justify-content: center;
+              }
+
+              p {
+                color: #999;
+                font-size: 12px;
+                margin: 0;
+                text-align: center;
+              }
+
+              .has-shadow + :global(.caption) {
+                margin: 0 0 40px;
+              }
+            `}</style>
+            <style jsx>{`
+              figure {
+                margin: ${margin}px 0;
+                text-align: ${align};
+              }
+
+              main {
+                ${align === 'center'
+                  ? 'margin: 0 auto'
+                  : align === 'right'
+                  ? 'margin-left: auto'
+                  : 'margin: 0'};
+              }
+
+              img {
+                ${borderRadius || shadow ? `border-radius: 5px;` : ''};
+                ${shadow ? `box-shadow: var(--shadow-large)` : ''};
+              }
+
+              @media (min-width: 992px) {
+                figure.oversize {
+                  width: ${width}px;
+                  margin: ${margin}px 0 ${margin}px
+                    calc(((${width}px - 768px) / 2) * -1);
+                }
+              }
+            `}</style>
+          </figure>
+        </IObserver>
+      </AmpImg>
+    )
+  }
+}
+
+// export const Video = props => <Image {...props} video />
+
+// export default Image
 
 const RemarkImage = ({
   caption,
@@ -8,22 +204,6 @@ const RemarkImage = ({
   components: customComponents,
   ...props
 }) => {
-  const Image = ({ src, width, height, title, alt }) => (
-    <figure>
-      <main>
-        <div>
-          <img
-            src={src}
-            width={width}
-            height={height}
-            title={title}
-            alt={alt}
-          />
-        </div>
-      </main>
-    </figure>
-  )
-
   return (
     <div className="remark-image">
       {href ? (
@@ -42,23 +222,7 @@ const RemarkImage = ({
         {caption}
       </RemarkCaption>
 
-      <style jsx>{`
-        div.remark-image {
-          margin: 40px auto;
-        }
-
-        div.remark-image :global(figure) {
-          text-align: center;
-        }
-
-        div.remark-image :global(figure main div img) {
-          width: unset;
-          max-width: 100%;
-          height: auto;
-          position: relative;
-          margin-bottom: 1.5rem;
-        }
-      `}</style>
+      <style jsx>{``}</style>
     </div>
   )
 }
