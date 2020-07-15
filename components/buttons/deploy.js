252f7b6b57438c663f89a3fff7893dd349c9db4a
mcsdevv <mail@mcs.dev>
2020-07-15 17:24:34 +0100

    account for no env on filter

--- components/buttons/deploy.js
+++ components/buttons/deploy.js
@@ -6,7 +6,7 @@ const VERCEL_EXAMPLES_URL = 'github.com/vercel/vercel/tree/master/examples/'
 export default function DeployButton({ env, envDescription, envLink, url }) {
   const formatEnv = () => {
     const envListFormatted =
-      env.filter(Boolean).length >= 2
+      env && env.filter(Boolean).length >= 2
         ? `&env=${env.filter(Boolean).join(',')}`
         : env[0] !== ''
         ? `&env=${env[0]}`
