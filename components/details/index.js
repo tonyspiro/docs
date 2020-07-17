16df205b142f9d99b39db62b99d9ffdd4303b1c3
timothyis <hello@timothy.is>
2020-07-17 08:14:44 +0100

    Detect amp and disable animated div for now

--- components/details/index.js
+++ components/details/index.js
@@ -1,4 +1,5 @@
 import React, { useRef, useEffect } from 'react'
+import { useAmp } from 'next/amp'
 import cn from 'classnames'
 
 import { AnimatedDiv } from './animated-container'
@@ -7,12 +8,13 @@ import styles from './details.module.css'
 
 function Details({ title, children, className, onClick, ...props }) {
   const animate = useRef(false)
+  const isAmp = useAmp()
 
   useEffect(() => {
     animate.current = true
   }, [])
 
-  return (
+  return !isAmp ? (
     <AnimatedDiv
       tension={290}
       friction={30}
@@ -33,6 +35,20 @@ function Details({ title, children, className, onClick, ...props }) {
         {children}
       </details>
     </AnimatedDiv>
+  ) : (
+    <details className={cn(styles.details, className)} {...props}>
+      <summary
+        onClick={e => {
+          if (onClick) {
+            e.preventDefault()
+            onClick(e)
+          }
+        }}
+      >
+        {title}
+      </summary>
+      {children}
+    </details>
   )
 }
 
