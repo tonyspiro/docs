fe4f218d1fb81e553d0e498357aba9691371cdaa
timothyis <hello@timothy.is>
2020-07-17 07:51:59 +0100

    Reverse amp check

--- components/deploy-button-generator/deploy-button-generator.js
+++ components/deploy-button-generator/deploy-button-generator.js
@@ -164,7 +164,7 @@ export default function DeployButtonGenerator() {
 
   return (
     <>
-      {isAmp && (
+      {!isAmp && (
         <>
           <Center>
             <Link href={completeUrl}>
